b329f2c5a4c21e0b0476db73d67c5799411ee911
Shyam-vadgama <Mr_Shyam_Vadgama@123>
2025-08-27 20:57:03 +0530

    first commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,6 @@
 import { MongoClient, type Db } from "mongodb"
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-}
+
 
 const uri = process.env.MONGODB_URI
 const options = {}
